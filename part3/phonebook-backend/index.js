6e17db53ea8d96b5b560527267fe4ade7701cf05
Safdar <71539665+malta-khan@users.noreply.github.com>
2023-06-10 06:42:27 +0500

    solved upto 3.5 phonebook backend

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,5 +1,6 @@
-const data = require("./data.json")
+let data = require("./data.json")
 const express = require("express")
+const uniqid = require("uniqid")
 const app = express()
 
 app.get("/",(request, response)=>{
@@ -9,9 +10,36 @@ app.get("/",(request, response)=>{
     <div><a href="/api/persons">/api/persons</a></div>`
     response.send(html)
 })
+
 app.get("/api/persons",(request, response)=>{
     response.send(data)
 })
+
+app.use(express.json())
+app.post("/api/persons",(request, response)=>{
+    let newPerson = request.body;
+    newPerson.id = uniqid()
+    data = data.concat(newPerson)
+    response.end()
+})
+
+app.get("/api/persons/:id",(request, response)=>{
+    let {id} = request.params;
+    let person = data.find(person=> person.id.toString() === id.toString())
+    if(person){
+        response.send(person)
+    }else{
+        response.statusMessage = "Invalid ID";
+        response.status(404).end("404 Invalid ID")
+    }
+})
+
+app.delete("/api/persons/:id",(request, response)=>{
+    let {id} = request.params;
+    data = data.filter(person=> person.id.toString() !== id.toString())
+    response.status(204).end()
+})
+
 app.get("/info",(request, response)=>{
     let date = new Date()
     let html = `
@@ -22,5 +50,5 @@ app.get("/info",(request, response)=>{
 
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on http://localhost:${PORT}/`)
 })
