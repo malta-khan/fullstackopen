6210c0355ec4b3493f83a0fbfacb56ae7ccd1410
Safdar <71539665+malta-khan@users.noreply.github.com>
2023-06-21 09:03:23 +0500

    Update index.js

--- part4/blogList/index.js
+++ part4/blogList/index.js
@@ -1,6 +1,6 @@
-const app = require("./app");
-const logger = require("./utils/logger");
 const config = require("./utils/config");
+const logger = require("./utils/logger");
+const app = require("./app");
 
 const PORT = config.PORT;
 app.listen(PORT, () => {
